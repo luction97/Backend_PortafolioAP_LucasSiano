c30882da830093941b0d242bd147a9c104239b83
Lucas Siano <88629712+luction97@users.noreply.github.com>
2022-12-18 10:09:27 -0300

    frontEnd actualizado

--- src/main/java/com/portafolioLS/AP/Controller/CExperiencia.java
+++ src/main/java/com/portafolioLS/AP/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendlucassiano.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
