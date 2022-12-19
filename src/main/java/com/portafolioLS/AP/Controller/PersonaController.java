13a457eefcb6b869b15db243d2f36fd994bbebb1
Lucas Siano <88629712+luction97@users.noreply.github.com>
2022-12-18 21:42:12 -0300

    Arreglo persona

--- src/main/java/com/portafolioLS/AP/Controller/PersonaController.java
+++ src/main/java/com/portafolioLS/AP/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -56,7 +56,7 @@ public class PersonaController {
         return persona;
     }
     
-    @GetMapping("personas/traer/perfil")
+    @GetMapping("/personas/traer/perfil")
     public Persona findPersona(){
         return ipersonaService.findPersona((long)1);
     }
